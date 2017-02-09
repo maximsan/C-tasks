c07d576a5df0779540296937647a2da8f45edb92
maximsan - Maksim Schutsky <maximuzz@yandex.ru>
2017-02-10 01:57:01 +0300

    FirstHomework

---
+++ readLetters/happyTicket/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace happyTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number of your ticket of tram:");
+            string strTicket = Console.ReadLine();
+            int iNumOfTicket = int.Parse(strTicket);
+            int iFirstNumeral = iNumOfTicket / 1000;
+            int iSecondNumeral = iNumOfTicket % 1000;
+            int iFirstSum = 0;
+            int iSecondSum = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                iFirstSum += iFirstNumeral % 10;
+                iFirstNumeral = iFirstNumeral / 10;
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                iSecondSum += iSecondNumeral % 10;
+                iSecondNumeral = iSecondNumeral / 10;
+            }
+            if (iFirstSum == iSecondSum)
+                Console.WriteLine("Your ticket is Happy!");
+            else
+            {
+                Console.WriteLine("Your ticket is decepiton!");
+            }
+
+        }
+    }
+}
---
+++ readLetters/parcerASCII/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parcerASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbolStr;
+            string newSymbolStr = null;
+            int iString;
+
+
+            while ((symbolStr = Console.ReadKey().KeyChar) != ' ')
+            {
+                if (char.IsUpper(symbolStr))
+                    //symbolStr = char.ToLower(symbolStr);
+                {
+                    iString = Convert.ToInt32(symbolStr) + 32;
+                    symbolStr = Convert.ToChar(iString);
+                }
+                else
+                    //symbolStr = char.ToUpper(symbolStr);
+                {
+                    iString = Convert.ToInt32(symbolStr) - 32;
+                    symbolStr = Convert.ToChar(iString);
+                }
+            newSymbolStr += symbolStr;
+            }
+            Console.WriteLine();
+            Console.WriteLine(newSymbolStr);
+        }
+    }
+}
