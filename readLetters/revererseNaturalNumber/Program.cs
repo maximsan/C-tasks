b804a446602c8adcdc81f0d65279c78f2396fc99
maximsan - Maksim Schutsky <maximuzz@yandex.ru>
2017-02-10 08:22:39 +0300

    First-Homework : all implementations

---
+++ readLetters/revererseNaturalNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace revererseNaturalNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strN;
+            Console.WriteLine("Inout number");
+            //strN = Console.ReadLine();
+            int N = int.Parse(strN = Console.ReadLine());
+            int uTemp, uNewN = 0;
+            do
+            {
+                uTemp = (N % 10);
+                N /= 10;
+                uNewN = (uNewN * 10 + uTemp);
+            } while (N != 0);
+            Console.WriteLine(uNewN);
+        }
+    }
+}
---
+++ readLetters/theNumeralEqualQuantity/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theNumeralEqualQuantity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input bottom border");
+            string strBottom = Console.ReadLine();
+            Console.WriteLine("Input upper border");
+            string strUpper = Console.ReadLine();
+            int iBottom = int.Parse(strBottom);
+            int iUpper = int.Parse(strUpper);
+            int iPrint, j;
+
+            for (int i = iBottom; i <= iUpper; i++)
+            {
+                iPrint = i;
+                j = i;
+                do
+                {
+                    Console.Write(iPrint);
+                    j--;
+                } while (j != 0);
+                Console.WriteLine();
+            }
+        }
+    }
+}
