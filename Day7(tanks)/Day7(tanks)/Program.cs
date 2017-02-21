826fec27d31853dccb676ec926d1436026007acc
maximsan - Maksim Schutsky <maximuzz@yandex.ru>
2017-02-21 20:04:29 +0300

    Homework3 - implementation

---
+++ Day7(tanks)/Day7(tanks)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib;
+using System.Threading;
+
+namespace Day7_tanks_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank [] panteraTanks = new Tank[5];
+            Tank [] t34Tanks = new Tank[5];
+            for (int i = 0; i < panteraTanks.Length; i++)
+            {
+                panteraTanks[i] = new Tank("Pantera - " + (i+1));
+                Thread.Sleep(100);
+            }
+ 
+            for (int i = 0; i < t34Tanks.Length; i++)
+            {
+                t34Tanks[i] = new Tank("T34 - " + (i+1));
+                Thread.Sleep(100);
+            }
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("In "+(i+1)+" battle win:");
+                Console.WriteLine(panteraTanks[i] * t34Tanks[i]);
+                Thread.Sleep(1000);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyClassLib/MyClassLib/Tank.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib
+{
+    public class Tank
+    {
+        private string NameOfTank;
+        private int LevelOfAllowanceOfAmmunition;
+        private int LevelOfArmor;
+        private int LevelOfMobility;
+        Random ran = new Random();
+
+        public Tank(string NameOfTank)
+        {
+            this.NameOfTank = NameOfTank;
+
+            this.LevelOfAllowanceOfAmmunition = Randomaizer(ran);
+            this.LevelOfArmor = Randomaizer(ran);
+            this.LevelOfMobility = Randomaizer(ran);
+        }
+
+        static int Randomaizer(Random ran)
+        {
+            return ran.Next(0, 100);
+        }
+
+        public void GetAllowanceOfAmmunition()
+        {
+            Console.WriteLine("Level of allowance of ammunition: {0}", LevelOfAllowanceOfAmmunition);
+        }
+
+        public void GetLevelOfArmor()
+        {
+            Console.WriteLine("Level of armor: {0}", LevelOfArmor);
+        }
+
+        public void GetLevelOfMobility()
+        {
+            Console.WriteLine("Level of mobility: {0}", LevelOfMobility);
+        }
+
+        public void GetNameOfTank()
+        {
+            Console.WriteLine("Name of tank: " + NameOfTank);
+        }
+        
+        public static string operator *(Tank panteraTank, Tank t34Tank)
+        {
+            if ((panteraTank.LevelOfAllowanceOfAmmunition * panteraTank.LevelOfArmor * panteraTank.LevelOfMobility >
+                t34Tank.LevelOfAllowanceOfAmmunition * t34Tank.LevelOfArmor * t34Tank.LevelOfMobility))
+                return panteraTank.NameOfTank;
+            else 
+                return t34Tank.NameOfTank;
+
+        }
+
+    }
+}
